a50761491e3b28cdd8177c4df01bf6f4b651346e
TimberedQuasar <szymon.j.jancewicz@gmail.com>
2025-12-06 22:12:28 +0100

    creating-project

---
+++ src/test/java/com/crm/crminventoryrepair/CrmInventoryRepairApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crm.crminventoryrepair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmInventoryRepairApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
